5a8bd16fc5b4003683d9bd12197e54c88f7128db
shivam mole <shivmole1713@gmail.com>
2024-09-02 12:23:40 +0530

    my project dgr

---
+++ src/EmpWedge.java
@@ -0,0 +1,12 @@
+public class EmpWedge {
+    public static void main(String args[]){
+        int IS_FULL_TIME = 1;
+        // Computation
+        double empCheck = Math. floor (Math. random () * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println ("Employee is Present");
+        else
+            System.out.println ("Employee is Abscent");
+
+    }
+}
