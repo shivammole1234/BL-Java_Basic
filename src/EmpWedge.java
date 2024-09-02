4c4bebd3aa8d5d5f8ec624c6d138d02bf03c7956
shivam mole <shivmole1713@gmail.com>
2024-09-02 15:40:01 +0530

    Test Case 4

--- src/EmpWedge.java
+++ src/EmpWedge.java
@@ -5,16 +5,21 @@ public class EmpWedge {
         int Wedge_Per_hr=20;
 
         int payable_wedge=0;
-        int emp_hr;
+        int emp_hr = 0;
         // Computation
         Random random = new Random();
-        double empCheck = Math.floor(Math.random () * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-            emp_hr=8;
-        else
-            emp_hr=4;
+        int empCheck = random.nextInt(2);
 
-        payable_wedge=emp_hr*Wedge_Per_hr;
+        switch(empCheck){
+            case 1:
+                emp_hr=8;
+                break;
+            case 0:
+                emp_hr=4;
+                break;
+        }
+
+        payable_wedge= emp_hr *Wedge_Per_hr;
         System.out.println("emp hr is :- "+emp_hr);
 
         System.out.println("Paybale amount of Employee is :- "+payable_wedge);
