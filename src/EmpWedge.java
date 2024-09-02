ae7b6ba0d0dce633b8c884012df742e16e32a859
shivam mole <shivmole1713@gmail.com>
2024-09-02 15:47:46 +0530

    Test Case 5

--- src/EmpWedge.java
+++ src/EmpWedge.java
@@ -5,24 +5,30 @@ public class EmpWedge {
         int Wedge_Per_hr=20;
 
         int payable_wedge=0;
-        int emp_hr = 0;
+        int emp_hr_per_day = 0;
+        int month_day=20;
         // Computation
         Random random = new Random();
         int empCheck = random.nextInt(2);
 
         switch(empCheck){
             case 1:
-                emp_hr=8;
+                emp_hr_per_day=8;
                 break;
             case 0:
-                emp_hr=4;
+                emp_hr_per_day=4;
                 break;
         }
 
-        payable_wedge= emp_hr *Wedge_Per_hr;
-        System.out.println("emp hr is :- "+emp_hr);
+        int payble_amount_month=emp_hr_per_day*Wedge_Per_hr*month_day;
+
+        payable_wedge= emp_hr_per_day *Wedge_Per_hr;
+        System.out.println("emp hr is :- "+emp_hr_per_day);
 
         System.out.println("Paybale amount of Employee is :- "+payable_wedge);
+        System.out.println("Paybale amount of Employee for a month is :- "+payble_amount_month);
+
+
 
     }
 }
